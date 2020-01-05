647f5e9c1ef094e78c20954ff92b4dbc0708685b
Narek Dallakyan <narek1994t@gmail.com>
2020-01-05 06:54:43 +0400

    Resolved Topic create logic.

--- am-ith-service/src/main/java/am/ith/service/service/course/impl/CourseServiceImpl.java
+++ am-ith-service/src/main/java/am/ith/service/service/course/impl/CourseServiceImpl.java
@@ -23,47 +23,41 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
 
-    private final CourseRepository courseRepository;
-    private final TrainerRepository trainerRepository;
-    private final TopicRepository topicRepository;
-    private final LevelRepository levelRepository;
-    private final CourseMapper courseMapper;
+  private final CourseRepository courseRepository;
+  private final TrainerRepository trainerRepository;
+  private final TopicRepository topicRepository;
+  private final LevelRepository levelRepository;
+  private final CourseMapper courseMapper;
 
-    @Override
-    public List<CourseResponse> courseResponseList() {
+  @Override
+  public List<CourseResponse> courseResponseList() {
 
-        List<Course> courses = courseRepository.findAll();
-        return courses
-                .stream()
-                .map(
-                        course ->
-                                courseMapper.
-                                        toCourseResponse(
-                                                course,
-                                                course.getTrainers(),
-                                                course.getLevels()))
-                .collect(Collectors.toList());
-    }
+    List<Course> courses = courseRepository.findAll();
+    return courses.stream()
+        .map(
+            course ->
+                courseMapper.toCourseResponse(course, course.getTrainers(), course.getLevels()))
+        .collect(Collectors.toList());
+  }
 
-    public CourseResponse getCourseById(Long id) {
-        List<Trainer> trainerList = trainerRepository.findAll();
-        List<Level> levelList = levelRepository.findAll();
-        CourseResponse courseResponse =
-                courseMapper.toCourseResponse(
-                        courseRepository.getOne(id), trainerList, levelList);
-        log.info("Get Course By Id after course mapper {}", courseResponse);
-        return courseResponse;
-    }
+  public CourseResponse getCourseById(Long id) {
+    List<Trainer> trainerList = trainerRepository.findAll();
+    List<Level> levelList = levelRepository.findAll();
+    CourseResponse courseResponse =
+        courseMapper.toCourseResponse(courseRepository.getOne(id), trainerList, levelList);
+    log.info("Get Course By Id after course mapper {}", courseResponse);
+    return courseResponse;
+  }
 
-    @Override
-    public CourseResponse createCourse(CourseRequest courseRequest) {
-        Course course = courseMapper.toCourseModel(courseRequest);
-        Course savedCourse = courseRepository.saveAndFlush(course);
-        return getCourseById(savedCourse.getId());
-    }
+  @Override
+  public CourseResponse createCourse(CourseRequest courseRequest) {
+    Course course = courseMapper.toCourseModel(courseRequest);
+    Course savedCourse = courseRepository.saveAndFlush(course);
+    return getCourseById(savedCourse.getId());
+  }
 
-    @Override
-    public CourseRequest updateCourse(String courseId, CourseRequest courseRequest) {
-        return null;
-    }
+  @Override
+  public CourseRequest updateCourse(String courseId, CourseRequest courseRequest) {
+    return null;
+  }
 }
